452f5f8a0318089985491ee044380d1bf0043f8c
Mauricio Vélez <serketh7@gmail.com>
2023-06-01 15:06:04 -0500

    add route to generate

--- api/Routs/index.js
+++ api/Routs/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const pokeRouter = require('./pokeRouts/pokeRouts');
 const exampleRouter = require('./pokeRouts/exampleRouts');
 
+const Pokemons = require('../services/pokemonsServices');
+
+const service = new Pokemons();
+
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
-  router.get('/', (req, res) => {
-    res.send('funcionando');
+  router.get('/', async (req, res) => {
+    await service.generate();
+    res.send('Pokemons added');
   });
   router.use('/pokemons', pokeRouter);
   router.use('/example', exampleRouter);
